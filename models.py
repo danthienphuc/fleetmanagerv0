0a7eb71a9229b67ba82301bfb88a274a61d16020
danthienphuc <danthienphuc@gmail.com>
2022-05-10 15:07:40 +0700

    new file db

--- models.py
+++ models.py
@@ -1,29 +1,31 @@
-from calendar import c
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
-BaseModel = declarative_base()
+from .database import Base
 
 
-class Fleet(BaseModel):
+class Fleet(Base):
     __tablename__ = "fleets"
-    id = Column(Integer, primary_key=True)
-    name = Column(String (50), nullable=False)
-    
-class Vehicle(BaseModel):
-    __tablename__ = "vehicles"
-    id = Column(Integer, primary_key=True)
-    name = Column(String (50), nullable=False)
-    fleet_id = Column(Integer, ForeignKey("fleets.id"))
 
-class Driver(BaseModel):
-    __tablename__ = "drivers"
-    id = Column(Integer, primary_key=True)
-    name = Column(String (50), nullable=False)
-    
-class Route(BaseModel):
-    __tablename__ = "routes"
-    id = Column(Integer, primary_key=True)
-    name = Column(String (50), nullable=False)
-        
-    
+    id = Column(Integer, primary_key=True,index=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
